6bf359ab1dcd4a2749b71c1e6f6aeced3b775659
tech_e <chris.durfee@dentalqore.com>
2025-02-24 14:59:39 -0700

    Add ExampleModule with activation method as a reference implementation

---
+++ modules/example/ExampleModule.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+namespace Modules\Example;
+
+use Proto\Module\Module;
+
+/**
+ * ExampleModule
+ *
+ * This module is an example of how to create a module in the Proto framework.
+ *
+ * @package Modules\Example
+ */
+class ExampleModule extends Module
+{
+    /**
+     * This will activate the module.
+     *
+     * @return void
+     */
+    public function activate(): void
+    {
+        // do something
+    }
+}
