7931e09bdd05bb03ce9558a1c9996839e591f00a
tech_e <chris.durfee@dentalqore.com>
2025-02-26 17:24:05 -0700

    Update user API routing to use UserController instead of ModelController

--- modules/user/api/api.php
+++ modules/user/api/api.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 namespace Modules\User\User\Api;
 
-use Proto\Controllers\ModelController;
+use Modules\User\Controllers\UserController;
 
-router()->resource('user', ModelController::class);
+router()->resource('user', UserController::class);
