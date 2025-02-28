649bd9033c457c28a3dba50d8af98d52bb3d9a6b
tech_e <chris.durfee@dentalqore.com>
2025-02-28 02:08:33 -0700

    Refactor generator cards and resource card components: update layout, remove unused props, and enhance click functionality

--- public/developer/src/components/pages/generator/generator-cards.js
+++ public/developer/src/components/pages/generator/generator-cards.js
@@ -5,39 +5,61 @@ import { ResourceCard } from "./resource-card.js";
 /**
  * GeneratorCards
  *
- * A section displaying generator cards.
+ * A section displaying the generator resource cards.
  *
  * @returns {object}
  */
-export const GeneratorCards = () => (
-    Div({ class: 'hidden md:flex flex-auto overflow-x-auto -mx-6 px-6 pb-2' }, [
-        Div({ class: 'flex flex-auto space-x-4 ml-[-24px] pl-6' }, [
-            ResourceCard({
-                title: 'Total Clients',
-                value: '1,200',
-                change: '+5.4% from last month',
-                icon: Icons.user.group
-            }),
-            ResourceCard({
-                title: 'New Clients',
-                value: '350',
-                change: '+12% from last month',
-                icon: Icons.user.plus
-            }),
-            ResourceCard({
-                title: 'Lost Clients',
-                value: '25',
-                change: '-3% from last month',
-                icon: Icons.user.minus
-            }),
-            ResourceCard({
-                title: 'Total Revenue',
-                value: '$145,678.00',
-                change: '+10% from last month',
-                icon: Icons.currency.dollar
-            }),
-        ])
-    ])
+export const GeneratorCards = () =>
+(
+	Div({ class: 'flex' }, [
+		Div({ class: 'grid grid-cols-4 gap-x-4 space-x-4' }, [
+			ResourceCard({
+				title: 'Full Resource',
+				click: () => {},
+				icon: Icons.document.duplicate
+			}),
+			ResourceCard({
+				title: 'API',
+				click: () => {},
+				icon: Icons.document.duplicate
+			}),
+			ResourceCard({
+				title: 'Controller',
+				click: () => {},
+				icon: Icons.document.duplicate
+			}),
+			ResourceCard({
+				title: 'Model',
+				click: () => {},
+				icon: Icons.document.duplicate
+			}),
+			ResourceCard({
+				title: 'Storage',
+				click: () => {},
+				icon: Icons.document.duplicate
+			}),
+			ResourceCard({
+				title: 'Policy',
+				click: () => {},
+				icon: Icons.document.duplicate
+			}),
+			ResourceCard({
+				title: 'Table',
+				click: () => {},
+				icon: Icons.document.duplicate
+			}),
+			ResourceCard({
+				title: 'Migration',
+				click: () => {},
+				icon: Icons.document.duplicate
+			}),
+			ResourceCard({
+				title: 'Unit Test',
+				click: () => {},
+				icon: Icons.document.duplicate
+			})
+		])
+	])
 );
 
 export default GeneratorCards;
