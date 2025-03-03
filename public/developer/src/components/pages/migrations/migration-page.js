6393572791c2e428ee15fd1da326400265423f8b
tech_e <chris.durfee@dentalqore.com>
2025-03-02 21:02:58 -0700

    Refactor MigrationPage to utilize MigrationModel for data handling and implement fetch method for improved data retrieval

--- public/developer/src/components/pages/migrations/migration-page.js
+++ public/developer/src/components/pages/migrations/migration-page.js
@@ -2,6 +2,7 @@ import { Div } from "@base-framework/atoms";
 import { BlankPage } from "@base-framework/ui/pages";
 import { clients } from "./clients.js"; // Import fake data
 import { MigrationTable } from "./migration-table.js";
+import { MigrationModel } from "./models/migration-model.js";
 import { PageHeader } from "./page-header.js";
 
 /**
@@ -10,7 +11,32 @@ import { PageHeader } from "./page-header.js";
  * @returns {object}
  */
 export const MigrationPage = () => (
-    new BlankPage([
+    new BlankPage({
+
+        /**
+         * @type {object}
+         */
+        data: new MigrationModel(),
+
+        /**
+         * This will fetch the data for the page.
+         *
+         * @param {number} offset
+         * @param {number} limit
+         * @param {function} callback
+         * @returns {void}
+         */
+        fetch(offset, limit, callback)
+        {
+            this.data.xhr.all(offset, limit, (response) =>
+            {
+                if (response)
+                {
+                    callback(response.rows);
+                }
+            });
+        }
+    }, [
         Div({ class: 'grid grid-cols-1' }, [
             Div({ class: 'flex flex-auto flex-col p-6 pt-0 lg:space-y-12 md:pt-6 lg:p-8 w-full mx-auto lg:max-w-7xl' }, [
                 PageHeader(),
