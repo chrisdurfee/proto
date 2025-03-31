7b13ff132a59c223801a33950d22bd4de137f8a8
tech_e <chris.durfee@dentalqore.com>
2025-03-31 16:56:53 -0600

    Add Permission API routes and PermissionController for role-based permissions management

---
+++ modules/user/api/role/permission/api.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+namespace Modules\User\Api\Role;
+
+use Modules\User\Controllers\PermissionController;
+
+/**
+ * Permission API Routes
+ *
+ * This file contains the API routes for the permission module.
+ */
+router()
+    ->resource('user/role/:roleId/permission', PermissionController::class);
---
+++ modules/user/controllers/PermissionController.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+namespace Modules\User\Controllers;
+
+use Proto\Controllers\ModelController as Controller;
+use Modules\User\Models\Permission;
+
+/**
+ * PermissionController
+ *
+ * @package Modules\User\Controllers
+ */
+class PermissionController extends Controller
+{
+
+	/**
+	 * Initializes the model class.
+	 *
+	 * @param string|null $modelClass The model class reference using ::class.
+	 */
+	public function __construct(protected ?string $modelClass = Permission::class)
+	{
+		parent::__construct($modelClass);
+	}
+}
