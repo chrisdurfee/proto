d12bd070c896365a7fcafaae2e11b5dbddc82b86
tech_e <chris.durfee@dentalqore.com>
2025-04-09 16:34:02 -0600

    feat(user-role): add UserRoleController for user role management and define API routes

--- modules/user/api/role/api.php
+++ modules/user/api/role/api.php
@@ -2,11 +2,20 @@
 namespace Modules\User\Api\Role;
 
 use Modules\User\Controllers\RoleController;
+use Modules\User\Controllers\UserRoleController;
+
+/**
+ * User Role API Routes
+ *
+ * This will handle the API routes for the User Roles.
+ */
+router()
+	->resource('user/:userId/role', UserRoleController::class);
 
 /**
  * Role API Routes
  *
- * This file contains the API routes for the Role module.
+ * This will handle the API routes for the Roles.
  */
 router()
-    ->resource('user/:userId/role', RoleController::class);
+	->resource('user/role', RoleController::class);
