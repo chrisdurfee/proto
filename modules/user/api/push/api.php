75caa2764953f4466e562219fc9d42ba54c9e33b
tech_e <chris.durfee@dentalqore.com>
2025-05-21 16:43:18 -0600

    feat: Refactor push API routes to group user-specific endpoints for better organization

--- modules/user/api/push/api.php
+++ modules/user/api/push/api.php
@@ -2,6 +2,7 @@
 namespace Modules\User\Api\Push;
 
 use Modules\User\Controllers\WebPushController;
+use Proto\Http\Router\Router;
 
 /**
  * Push API Routes
@@ -9,5 +10,9 @@ use Modules\User\Controllers\WebPushController;
  * This file contains the API routes for the push notifications.
  */
 router()
-	->post('push/subscribe', [WebPushController::class, 'subscribe'])
-    ->post('push/unsubscribe', [WebPushController::class, 'unsubscribe']);
+	->group('user/{id}', function(Router $router)
+	{
+		$router
+			->post('push/subscribe', [WebPushController::class, 'subscribe'])
+			->post('push/unsubscribe', [WebPushController::class, 'unsubscribe']);
+	});
