3e5742bdabc135b5faa0770df1ea0d437b7624cf
tech_e <chris.durfee@dentalqore.com>
2025-05-21 15:53:19 -0600

    feat: Implement send method in WebPushUserController to handle web push notifications

--- modules/user/controllers/WebPushUserController.php
+++ modules/user/controllers/WebPushUserController.php
@@ -3,6 +3,7 @@ namespace Modules\User\Controllers;
 
 use Proto\Controllers\ResourceController as Controller;
 use Modules\User\Models\WebPushUser;
+use Proto\Dispatch\Dispatcher;
 
 /**
  * WebPushUserController
@@ -19,5 +20,31 @@ class WebPushUserController extends Controller
 	public function __construct(protected ?string $modelClass = WebPushUser::class)
 	{
 		parent::__construct();
+	}
+
+	/**
+	 * Sends a web push notification to the user.
+	 *
+	 * @param mixed $userId The user ID to send the notification to.
+	 * @param object $settings The settings for the notification.
+	 * @param object|null $data Optional data for the notification.
+	 * @return object|null The response from the dispatcher or null if user ID is not set.
+	 */
+	public function send(mixed $userId, object $settings, ?object $data = null): ?object
+	{
+		if (!isset($userId))
+		{
+			return null;
+		}
+
+		$model = new ($this->modelClass)();
+		$subscriptions = $model->getByUser($userId);
+		if (!isset($subscriptions))
+		{
+			return null;
+		}
+
+		$settings->subscriptions = $subscriptions;
+		return Dispatcher::push($settings, $data);
 	}
 }
