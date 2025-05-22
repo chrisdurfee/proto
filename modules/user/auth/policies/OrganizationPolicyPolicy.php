2a3bfa4267515191b506498d81a43f8050f7fcae
tech_e <chris.durfee@dentalqore.com>
2025-05-22 12:05:44 -0600

    feat: Add OrganizationPolicyPolicy and SeedOrganizationPermission migration for organization permissions

---
+++ modules/user/auth/policies/OrganizationPolicyPolicy.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace Modules\User\Policies;
+
+use Proto\Auth\Policies\Policy;
+
+/**
+ * OrganizationPolicyPolicy
+ * 
+ * @package Modules\User\Policies
+ */
+class OrganizationPolicyPolicy extends Policy
+{
+
+}
