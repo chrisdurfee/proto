aa414475ab2d3481dc4e77574ec3a36ebcc5e50d
tech_e <chris.durfee@dentalqore.com>
2025-07-02 15:26:27 -0600

    feat: Add TextSection component and enhance HomePage with new content sections

--- apps/developer/src/components/pages/home/components/pages/home-page.js
+++ apps/developer/src/components/pages/home/components/pages/home-page.js
@@ -1,4 +1,4 @@
-import { A, Code, Div, H1, H3, Header, P, Pre } from '@base-framework/atoms';
+import { A, Code, Div, H1, H3, Header, P, Pre, Section } from '@base-framework/atoms';
 import { Atom } from '@base-framework/base';
 import { Icons } from "@base-framework/ui/icons";
 import { BlankPage } from "@base-framework/ui/pages";
@@ -9,9 +9,9 @@ import { BlankPage } from "@base-framework/ui/pages";
  * @returns {object}
  */
 const MainButtonContainer = Atom(() => (
-    Div({ class: 'mt-10 flex items-center justify-start gap-x-4 px-6 pb-6' }, [
+    Div({ class: 'mt-10 flex items-center justify-start gap-x-4 px-6 pb-6 border-b' }, [
 		A({
-			href: '/generator',
+			href: '/docs',
 			class: 'bttn primary'
 		}, 'Get started')
 	])
@@ -44,6 +44,22 @@ export const SectionHeader = Atom(({ title, description}) => (
 ));
 
 /**
+ * This will create a text section.
+ *
+ * @param {object} props
+ * @returns {object}
+ */
+const TextSection = Atom(({ title, description }, children) => (
+	Section({ class: 'flex flex-col w-full px-6 py-8 border-b' }, [
+		SectionHeader({
+			title,
+			description
+		}),
+		children
+	])
+));
+
+/**
  * This will create a main section.
  *
  * @param {object} props
@@ -52,11 +68,11 @@ export const SectionHeader = Atom(({ title, description}) => (
 const MainSection = Atom((props, children) => (
 	Div({ class: 'flex flex-auto flex-col' }, [
 		Div({
-			class: 'flex flex-auto flex-col w-full max-w-[1400px] m-auto sm:pt-8'
+			class: 'flex flex-auto flex-col w-full max-w-[1400px] m-auto sm:pt-8 lg:border-r lg:border-l'
 		}, [
 			PageHeader({
-				title: 'Developer Utils',
-				description: 'A collection of developer tools to help you build better applications.'
+				title: 'Build faster with Proto and Base',
+				description: 'Proto is a well designed framework that provides a set of tools and utilities to help you build applications faster and more efficiently.'
 			}),
 			MainButtonContainer(),
 			...children
@@ -97,7 +113,45 @@ const CodeCard = Atom((props, children) => (
 export const HomePage = () => (
 	new BlankPage([
 		MainSection([
-
+			TextSection({
+				title: 'Why Proto?',
+				description: 'Proto is built to simplify the development process. It\'s modular, supports small and large teams, and has many of the tools needed out of the box. It\'s also open source.'
+			}, [
+				CodeCard('composer install protoframework/proto'),
+				Div({ class: 'mt-10 flex items-center justify-start gap-x-4 pb-6' }, [
+					A({
+						href: 'https://github.com/chrisdurfee/proto',
+						target: '_blank',
+						class: 'bttn ghost gap-2'
+					}, 'Proto Source')
+				])
+			]),
+			TextSection({
+				title: 'Security',
+				description: 'Proto comes with built-in security features to help you protect your application. It includes support for authentication, authorization, and data protection. Gates, policies, and middleware are all included to help you secure your application.'
+			}),
+			TextSection({
+				title: 'Code Generation',
+				description: 'Proto comes with built-in code generation features to help you quickly scaffold your application. It includes support for generating full modules, gateways, controllers, models, and more.'
+			}, [
+				Div({ class: 'mt-10 flex items-center justify-start gap-x-4 pb-6' }, [
+					A({
+						href: '/generator',
+						class: 'bttn ghost gap-2'
+					}, 'Use Generator')
+				])
+			]),
+			TextSection({
+				title: 'Error Tracking',
+				description: 'Proto has built-in error tracking features to help you monitor and fix issues in your application. It includes support for logging, monitoring, and alerting.'
+			}, [
+				Div({ class: 'mt-10 flex items-center justify-start gap-x-4 pb-6' }, [
+					A({
+						href: '/errors',
+						class: 'bttn ghost gap-2'
+					}, 'View Errors')
+				])
+			]),
 		])
 	])
 );
