c1d7914d47f3115640345094e683b96f115f8106
tech_e <chris.durfee@dentalqore.com>
2025-07-04 02:56:30 -0600

    feat: Enhance forgot password form with loading state and email caching

--- apps/crm/src/shell/login/forms/forgot-password-form.js
+++ apps/crm/src/shell/login/forms/forgot-password-form.js
@@ -1,7 +1,8 @@
-import { Div } from '@base-framework/atoms';
-import { Button, EmailInput } from "@base-framework/ui/atoms";
+import { Div, OnState } from '@base-framework/atoms';
+import { Button, EmailInput, LoadingButton } from "@base-framework/ui/atoms";
 import { Form } from '@base-framework/ui/molecules';
-import { STEPS } from '../steps';
+import { AuthModel } from '../../../../../common/models/auth-model.js';
+import { STEPS } from '../steps.js';
 
 /**
  * This will create the email container.
@@ -11,10 +12,34 @@ import { STEPS } from '../steps';
 const EmailContainer = () => (
 	Div({ class: 'grid gap-4' }, [
 		Div({ class: 'grid gap-4' }, [
-			EmailInput( { placeholder: 'Email Address', required: true, 'aria-required': true } ),
+			EmailInput({
+				cache: 'email',
+				placeholder: 'Email Address',
+				required: true,
+				'aria-required': true
+			}),
 		]),
 	])
 );
+
+/**
+ * Requests a verification code for the selected multi-factor authentication option.
+ *
+ * @param {object} parent - The parent component.
+ */
+const requestPasswordReset = (parent) =>
+{
+	parent.state.loading = true;
+	const model = new AuthModel({
+		email: parent.email.value
+	});
+
+	model.xhr.requestPasswordReset('', (response) =>
+	{
+		parent.state.loading = false;
+		parent.state.showMessage = true;
+	});
+};
 
 /**
  * This will create the submit button.
@@ -23,7 +48,10 @@ const EmailContainer = () => (
  */
 const SubmitButton = () => (
 	Div({ class: 'grid gap-4' }, [
-		Button({ type: 'submit' }, 'Submit')
+		OnState('loading', (state) => (state)
+			? LoadingButton({ disabled: true })
+			: Button({ type: 'submit' }, 'Submit')
+		)
 	])
 );
 
@@ -44,7 +72,7 @@ const CancelButton = () => (
  * @returns {object}
  */
 export const ForgotPasswordForm = () => (
-	Form({ class: 'flex flex-col p-6 pt-0', submit: (e, parent) => parent.state.showMessage = true, role: 'form' }, [
+	Form({ class: 'flex flex-col p-6 pt-0', submit: (e, parent) => requestPasswordReset(parent), role: 'form' }, [
 		Div({ class: 'grid gap-4' }, [
 			EmailContainer(),
 			SubmitButton(),
