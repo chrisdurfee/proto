6336e750d4dd23b488d4b24dc52dbe10e10597db
tech_e <chris.durfee@dentalqore.com>
2025-07-16 14:39:41 -0600

    fix: refine return handling in storage normalization and remove deprecated where method

--- proto/models/Model.php
+++ proto/models/Model.php
@@ -707,6 +707,16 @@ abstract class Model extends Base implements \JsonSerializable, ModelInterface
 			return $result;
 		}
 
+		if (is_int($result) || is_float($result))
+		{
+			return $result;
+		}
+
+		if (is_string($result))
+		{
+			return $result;
+		}
+
 		return $model->storage->normalize($result);
 	}
 
@@ -1016,21 +1026,6 @@ abstract class Model extends Base implements \JsonSerializable, ModelInterface
 		}
 
 		return $row;
-	}
-
-	/**
-	 * Get rows from storage.
-	 *
-	 * @param array $filter Filter criteria.
-	 * @return AdapterProxy
-	 */
-	public static function where(array $filter): AdapterProxy
-	{
-		$instance = new static();
-		/**
-		 * @SuppressWarnings PHP0408,PHP0423
-		 */
-		return $instance->storage->where($filter);
 	}
 
 	/**
