3c3debc79ea1f2a23f1a7abcfe749b9aded4ad3b
tech_e <chris.durfee@dentalqore.com>
2025-09-02 22:55:48 -0600

    fix: Add restrictFields method to remove specified fields from data

--- src/Controllers/ApiController.php
+++ src/Controllers/ApiController.php
@@ -34,6 +34,21 @@ abstract class ApiController extends Controller
 	}
 
 	/**
+	 * Restricts the fields from the given data.
+	 *
+	 * @param object $data The data to restrict.
+	 * @param array $fields The fields to restrict.
+	 * @return void
+	 */
+	protected function restrictFields(object &$data, array $fields = []): void
+	{
+		foreach ($fields as $field)
+		{
+			unset($data->$field);
+		}
+	}
+
+	/**
 	 * Validates the request data.
 	 *
 	 * This method can be overridden in subclasses to provide specific validation logic.
