2d7941f9f2d4d5ed854aa6592c64136b43d6c887
tech_e <chris.durfee@dentalqore.com>
2026-04-02 17:06:25 -0600

    refactor(SyncController): remove unused methods and integrate SyncableTrait

--- src/Controllers/SyncController.php
+++ src/Controllers/SyncController.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 namespace Proto\Controllers;
 
-use Proto\Http\Router\Request;
+use Proto\Controllers\Traits\SyncableTrait;
 
 /**
  * SyncController
@@ -33,39 +33,5 @@ use Proto\Http\Router\Request;
  */
 abstract class SyncController extends ApiController
 {
-	/**
-	 * Start the SSE stream.
-	 *
-	 * Sets up the Redis subscription on the channel returned by getChannel()
-	 * and dispatches messages through handleMessage().
-	 *
-	 * @param Request $request The incoming request.
-	 * @return void
-	 */
-	public function sync(Request $request): void
-	{
-		$channel = $this->getChannel($request);
-		redisEvent($channel, fn(string $ch, array $msg) => $this->handleMessage($ch, $msg, $request));
-	}
-
-	/**
-	 * Get the Redis channel name(s) for this SSE endpoint.
-	 *
-	 * @param Request $request The incoming request (use to extract route params).
-	 * @return string|array Single channel name or array of channel names.
-	 */
-	abstract protected function getChannel(Request $request): string|array;
-
-	/**
-	 * Handle an incoming Redis message.
-	 *
-	 * Return an associative array to send as an SSE event, or null to skip.
-	 * Return false to terminate the connection.
-	 *
-	 * @param string $channel The channel the message was received on.
-	 * @param array $message The decoded message payload.
-	 * @param Request $request The original HTTP request.
-	 * @return array|null|false
-	 */
-	abstract protected function handleMessage(string $channel, array $message, Request $request): array|null|false;
+	use SyncableTrait;
 }
